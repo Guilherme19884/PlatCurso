a914714e887e389001ac127a3db63e94f3a4d26c
Guilherme Santos Araújo <guilherme1984.araujo@gmail.com>
2023-05-29 07:23:21 -0300

    controllers concluido

--- Controllers/AdministradorController .cs
+++ Controllers/AdministradorController .cs
@@ -27,5 +27,61 @@ namespace PlataformaDeCurso.Controllers
             _context.SaveChanges();
             return Ok(administrador);
         }
+
+        [HttpGet]
+        public IActionResult ListarAdministrador()
+        {
+            return Ok(_context.cursos);
+        }
+
+        [HttpGet("listarCurso/{id}")]
+        public IActionResult listarAdministradorPorId(int id)
+        {
+            var Administrador = _context.administradors.Find(id);
+            if (Administrador == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Administrador);
+            }
+        }
+
+        [HttpGet("obterPorNome")]
+        public IActionResult ListarPorNome(string nome)
+        {
+            var Administrador = _context.administradors.Where(x => x.nome.Contains(nome));
+            return Ok(Administrador);
+        }
+
+        [HttpPut("atualizar/{id}")]
+        public IActionResult Atualizar(int id, Administrador administrador)
+        {
+            var Administrador = _context.administradors.Find(id);
+            if (Administrador == null)
+            {
+                return NotFound();
+            }
+            Administrador.Id = Administrador.Id;
+            Administrador.nome = Administrador.nome;
+
+            _context.administradors.Update(administrador);
+            _context.SaveChanges();
+            return Ok(Administrador);
+        }
+
+        [HttpDelete("deletar/{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var administradorBanco = _context.administradors.Find(id);
+            if (administradorBanco == null)
+            {
+                return NotFound();
+            }
+            _context.administradors.Remove(administradorBanco);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -3,11 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PlataformaDeCurso.Contexto;
+using PlataformaDeCurso.Model;
 
 namespace PlataformaDeCurso.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UsuarioController : ControllerBase
     {
+        private readonly AppContexto _context;
 
+        public UsuarioController(AppContexto context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarAdministrador(Usuario usuario)
+        {
+            _context.Add(usuario);
+            _context.SaveChanges();
+            return Ok(usuario);
+        }
+
+        [HttpGet]
+        public IActionResult ListarUsuario()
+        {
+            return Ok(_context.cursos);
+        }
+
+        [HttpGet("listarCurso/{id}")]
+        public IActionResult listarUsuarioPorId(int id)
+        {
+            var Usuario = _context.usuarios.Find(id);
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Usuario);
+            }
+        }
+
+        [HttpGet("obterPorNome")]
+        public IActionResult ListarPorNome(string nome)
+        {
+            var Usuario = _context.administradors.Where(x => x.nome.Contains(nome));
+            return Ok(Usuario);
+        }
+
+        [HttpPut("atualizar/{id}")]
+        public IActionResult Atualizar(int id, Usuario usuario)
+        {
+            var Usuario = _context.usuarios.Find(id);
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+            Usuario.Id = Usuario.Id;
+            Usuario.nome = Usuario.nome;
+
+            _context.usuarios.Update(usuario);
+            _context.SaveChanges();
+            return Ok(Usuario);
+        }
+
+        [HttpDelete("deletar/{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var usuarioBanco = _context.usuarios.Find(id);
+            if (usuarioBanco == null)
+            {
+                return NotFound();
+            }
+            _context.usuarios.Remove(usuarioBanco);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
